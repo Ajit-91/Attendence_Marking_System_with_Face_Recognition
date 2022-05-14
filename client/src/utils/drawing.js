5d2d500a032990f18980b36f5ca6686168ecefc6
Ajit-91 <ajitkumar12105@gmail.com>
2022-05-14 12:56:47 +0530

    changed folder structure

--- client/src/utils/drawing.js
+++ client/src/utils/drawing.js
@@ -6,14 +6,12 @@ export const drawRect = (detections, ctx) =>{
       const [x, y, width, height] = prediction['bbox']; 
       const text = prediction['class']; 
   
-      // Set styling
-      const color = Math.floor(Math.random()*16777215).toString(16);
-      ctx.strokeStyle = '#' + color
+      ctx.strokeStyle = 'red'
       ctx.font = '18px Arial';
   
       // Draw rectangles and text
       ctx.beginPath();   
-      ctx.fillStyle = '#' + color
+      ctx.fillStyle = 'red'
       ctx.fillText(text, x, y);
       ctx.rect(x, y, width, height); 
       ctx.stroke();
